3ca7b5d3971382e657d6bb64859d1ff589e910e4
S567006 <143199322+S567006@users.noreply.github.com>
2023-10-25 22:20:42 -0500

    Update mydata.js

--- routes/mydata.js
+++ routes/mydata.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Tarun Yada' });
+  res.render('mydata', { title: 'Tarun Yada' });
 });
 
 module.exports = router;
